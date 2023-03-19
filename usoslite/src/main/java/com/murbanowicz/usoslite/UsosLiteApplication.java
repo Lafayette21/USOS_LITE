ab0f3c6bbb4dd3fec59297a86aad7eb22d8b3a7d
Marcin Urbanowicz <thmarcin@student.agh.edu.pl>
2023-03-19 21:24:33 +0100

    Make some basic configurations

--- usoslite/src/main/java/com/murbanowicz/usoslite/UsosLiteApplication.java
+++ usoslite/src/main/java/com/murbanowicz/usoslite/UsosLiteApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UsosliteApplication {
+public class UsosLiteApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UsosliteApplication.class, args);
+		SpringApplication.run(UsosLiteApplication.class, args);
 	}
 
 }
