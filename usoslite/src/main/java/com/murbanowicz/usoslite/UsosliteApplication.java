441406ad148440c557a79e7359400e9e4f498439
Marcin Urbanowicz <thmarcin@student.agh.edu.pl>
2023-03-19 18:34:43 +0100

    Setup project structure

---
+++ usoslite/src/main/java/com/murbanowicz/usoslite/UsosliteApplication.java
@@ -0,0 +1,13 @@
+package com.murbanowicz.usoslite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsosliteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsosliteApplication.class, args);
+	}
+
+}
---
+++ usoslite/src/test/java/com/murbanowicz/usoslite/UsosliteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murbanowicz.usoslite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsosliteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
